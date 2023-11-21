35c961b8e7755b75ee7ec18ff91f300f6e0d8cf3
Miki, Hiromitsu <mikiepure+dev@gmail.com>
2023-11-21 23:42:43 +0900

    Fix version and Copyright

--- WinRLauncher/Properties/AssemblyInfo.cs
+++ WinRLauncher/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WinR Launcher")]
-[assembly: AssemblyCopyright("Copyright ©  2015 Miki, Hiromitsu")]
+[assembly: AssemblyCopyright("Copyright (c) 2015 Miki, Hiromitsu")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyVersion("0.2.1")]
